bde61ba11e4b701e02d7b5c4d46e1c1a282eda30
Bikash Chauhan <chauhanbikash789@gmail.com>
2024-01-09 23:49:28 +0530

    Updated favicon and Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <Link className="navbar-brand text-uppercase" to="/">
-            QWERTY
+            NewsFlash
           </Link>
           <button
             className="navbar-toggler"
